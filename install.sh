a45f5133b0b49ff4329467d8977550a96fe5e9e4
Evan Pratten <ewpratten@gmail.com>
2018-08-19 21:11:49 -0400

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,7 @@
+cd ~/Downloads
+wget https://github.com/zdevito/terra/releases/download/release-2016-03-25/terra-Linux-x86_64-332a506.zip
+unzip terra-Linux-x86_64-332a506.zip
+cd terra-Linux-x86_64-332a506
+cp -r ./* /usr/local/
+cd ..
+rm -rf ./terra-Linux-x86_64-332a506
